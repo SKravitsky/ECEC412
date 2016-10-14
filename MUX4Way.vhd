8a86a1357cdb91bc957e57276b6262adf72e95c2
Jayx239 <grazinpalm@gmail.com>
2016-10-14 14:26:53 -0400

    Made changes

--- MUX4Way.vhd
+++ MUX4Way.vhd
@@ -9,25 +9,25 @@ entity MUX4Way is
   );
 end MUX4Way;
 
-architecture MUX4Way_Imp of MUX4Way is
-	signal outsig: std_logic_vector(31 downto 0);
+architecture Structural of MUX4Way is
+signal outsig: std_logic_vector(31 downto 0);
+begin
 	
-	begin
-	
-	process(sel)
-	begin
-	case sel is
-		when "00" =>
-			outsig <= v;
-		when "01" =>
-			outsig <= w;
-		when "10" =>
-			outsig <= x;
-		when "11" =>
-			outsig <= y;
-		when others =>
-			outsig <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-	end case;
-	z <= outsig;
-	end process;
-end MUX4Way_Imp;
+  process(sel)
+  begin
+    case sel is
+      when "00" =>
+        outsig <= v;
+      when "01" =>
+        outsig <= w;
+      when "10" =>
+        outsig <= x;
+      when "11" =>
+        outsig <= y;
+      when others =>
+        outsig <= X"00000000";
+      end case;
+      z <= outsig;
+  end process;
+
+end Structural;
