8d7de529a4c3bae33f9b3878f5d6229d8483f220
Jayx239 <grazinpalm@gmail.com>
2016-11-26 16:35:36 -0500

    Added write register address, alu result, and memory read data

--- MEMWBRegister.vhd
+++ MEMWBRegister.vhd
@@ -3,21 +3,29 @@ use ieee.std_logic_1164.all;
 
 entity MEMWBRegister is
   port(
-    regwr_in, memtoreg_in: in std_logic;
-    clk: in std_logic;
+    memreaddata_in, wrregaddress_in, aluresult_in: std_logic_vector(31 downto 0);
+    regwr_in, memtoreg_in,clk: in std_logic;
+    memreaddata_out, wrregaddress_out, aluresult_out: out std_logic_vector(31 downto 0);
     regwr_out, memtoreg_out: out std_logic
   );
 end MEMWBRegister;
 
 architecture Structural of MEMWBRegister is
+signal memreaddata, wrregaddress, aluresult: std_logic_vector(31 downto 0);
 signal regwr, memtoreg: std_logic;
 begin
   process(clk)
   begin
     if rising_edge(clk) then
+        memreaddata_out <= memreaddata;
+        wrregaddress_out <= wrregaddress;
+        aluresult_out <= aluresult;
     	regwr_out <= regwr;
     	memtoreg_out <= memtoreg;
     else
+      memreaddata <= memreaddata_in;
+      wrregaddress <= wrregaddress_in;
+      aluresult <= aluresult_in;
       regwr <= regwr_in;
       memtoreg <= memtoreg_in;
     end if;
